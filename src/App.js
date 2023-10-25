fb9a52dfa2d2ac2747b34e4518296e280267cd8d
thisisaayush <thakurikarthik@gmail.com>
2023-10-24 19:35:02 -0500

    added profile component features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import Profile from './PortfolioContainer/Home/Profile';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 import './App.css';
 
 function App() {
+  // const [typeEffect] = useTypewriter({
+  //   words: ['Software Engineer', 'Full-Stack Developer', '.Net Developer'],
+  //   loop:{},
+  //   typeSpeed: 100,
+  //   deleteSpeed: 40
+  // })
   return (
     <div className="App">
-      
+    {/* //   <h4 style={{color: 'green'}}>
+    //     I'm a 
+    //   </h4>
+    //     <span style={{fontweight: 'bold', color:'green', marginleft:'5px'}}>{typeEffect}</span> */}
+        <Profile></Profile>
     </div>
   );
 }
---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import Typical from 'react-typical'
+
+export default function Profile() {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-details'>
+                    <div className='colz'>
+                        <div className='colz-icon'>  
+                            <a href='https://www.linkedin.com/in/thisisaayushthakuri/'>
+                                <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+                            </a>
+                            <a href='#'>
+                                <i className="fa fa-google-plus-square" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    
+                        <div className="profile-details-name">
+                            <span className="primary-text">
+                                {" "}
+                                Hello, I'm <span className="highlighted-text">Aayush</span>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="profile-details-role">
+                        <span className="primary-text">
+                            {" "}
+                            <h1>
+                                {" "}
+                                <Typical
+                                    loop={Infinity}
+                                    steps={[
+                                        "Software Engineer", 4000,
+                                        "Application Developer", 4000,
+                                        "Full-Stack Developer", 4000,
+                                    ]}>
+
+                                </Typical>
+                            </h1>
+                            <span className='profile-role-tagline'>
+                                Diverse experience in building full-stack applications.
+                            </span>
+                        </span>
+                    </div>
+                    <div className='profile-optins'>
+                        <button className='btn primary-btn'>
+                            {" "}
+                            Hire Me{" "}
+                        </button>
+                        <a href='Aayush Thakuri Resume.pdf' download='Aayush Thakuri Resume.pdf'>
+                            <button className='btn highlighted-btn'>Get Resume</button>
+                        </a>
+                    </div>
+                </div>
+                <div className='profile-picture'>
+                    <div className='profile-picture-background'>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
