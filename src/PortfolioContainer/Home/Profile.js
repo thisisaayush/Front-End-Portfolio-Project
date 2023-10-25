aa73839fa4d6e304ac0971b81512e8e8f1cda069
thisisaayush <thakurikarthik@gmail.com>
2023-10-25 13:49:11 -0500

    added css in  profile commponents sections

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -45,7 +45,7 @@ export default function Profile() {
                             </span>
                         </span>
                     </div>
-                    <div className='profile-optins'>
+                    <div className='profile-options'>
                         <button className='btn primary-btn'>
                             {" "}
                             Hire Me{" "}
