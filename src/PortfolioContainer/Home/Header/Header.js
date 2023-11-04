1ec23c7f3c22b244f34a1a273cabd9bde8cec21a
thisisaayush <thakurikarthik@gmail.com>
2023-11-04 11:13:19 -0500

    resolved the color issue

--- src/PortfolioContainer/Home/Header/Header.js
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -9,44 +9,42 @@ export default function Header() {
     const [selectedScreen, setSelectedScreen] = useState(0);
     const [showHeaderOptions, setShowHeaderOptions] = useState(false);
 
-    const updatedCurrentScreen = (currentScreen) => {
-        if (!currentScreen || !currentScreen.screenInView) {
+    const updateCurrentScreen = (currentScreen) => {
+        if (!currentScreen || !currentScreen.screenInView) 
             return;
-        }
+        
         let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView);
-        if (screenIndex < 0) {
+        if (screenIndex < 0) 
             return;
-        }
-    };
-    let currentScreenSubscription = ScrollService.currentScreenBroadCaster.subscribe(updatedCurrentScreen);
+        
+    }
+    let currentScreenSubscription = ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
 
     const getHeaderOptions = () => {
         return (
             TOTAL_SCREENS.map((screen, i) => (
-                <div key={screen.screen_name} className={getHeaderOptionsClass(i)} onClick={() => switchScreen(i, screen)}>
+                <div key={screen.screen_name} className={getHeaderOptionsClass(i)} 
+                     onClick={() => switchScreen(i, screen)}>
                     <span>{screen.screen_name}</span>
                 </div>
             ))
-        );
+        )
     }
 
     const getHeaderOptionsClass = (index) => {
         let classes = "header-option";
-        if (index < TOTAL_SCREENS.length - 1) {
-            classes += "header-option-separator"; 
-        }
-
-        if (selectedScreen === index) {
-            classes += " selected-header-option";
-        }
+        if (index < TOTAL_SCREENS.length - 1) 
+        classes += "header-option-seperator" 
+        
+        if (selectedScreen === index) 
+        classes += " selected-header-option";
         return classes;
     };
 
     const switchScreen = (index, screen) => {
         let screenComponent = document.getElementById(screen.screen_name);
-        if (!screenComponent) {
-            return;
-        }
+        if (!screenComponent) 
+        return;
 
         screenComponent.scrollIntoView({ behavior: "smooth" });
         setSelectedScreen(index);
