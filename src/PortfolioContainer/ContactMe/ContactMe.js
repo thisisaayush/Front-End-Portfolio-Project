ae36d7e79328b47fcff90a475a8804797c626887
thisisaayush <thakurikarthik@gmail.com>
2023-11-08 16:55:18 -0600

    added css for contactme component

--- src/PortfolioContainer/ContactMe/ContactMe.js
+++ src/PortfolioContainer/ContactMe/ContactMe.js
@@ -5,6 +5,7 @@ import load1 from '../../../src/images/load2.gif'
 import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
 import ScrollService from '../../utilities/ScrollService';
 import Animations from '../../utilities/Animations';
+import './ContactMe.css'
 
 export default function ContactMe(props) {
 
